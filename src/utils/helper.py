815420d5e2d727fc3ed233319b009bb6c32ede65
daehan <dh16931@gmail.com>
2022-01-03 16:43:59 +0900

    style: add space in code

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,4 +1,4 @@
 from os import path, listdir
 
 def get_files_from_dir(data_path):
-    return[f for f in listdir(data_path) if path.isfile(path.join(data_path, f))]
+    return [f for f in listdir(data_path) if path.isfile(path.join(data_path, f))]
