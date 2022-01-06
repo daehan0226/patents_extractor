0809fb7b94b8d9a0daf6e798739078a4af79ab96
daehan <dh16931@gmail.com>
2022-01-06 17:51:27 +0900

    test: change unittest to pytest

--- test/test_helper.py
+++ test/test_helper.py
@@ -1,38 +1,26 @@
-import unittest
 import os
 import shutil
-from os.path import isfile
 from datetime import datetime
+import pytest
 from src.utils.helper import convert_to_datetime, get_files_from_dir
 from src.utils.error import StringDateFormat
 
-class DatetimeHandlerTests(unittest.TestCase): 
+def test_convert_to_datetime():
+    assert(convert_to_datetime("2022.01.05") == datetime(year=2022, month=1, day=5))
+    
+def test_convert_to_datetime_raise_error():
+    with pytest.raises(StringDateFormat):
+        convert_to_datetime("01.05.2022")
 
-    def test_convert_to_datetime(self):
-        self.assertEqual(convert_to_datetime("2022.01.05"), datetime(year=2022, month=1, day=5))
+def test_get_files_from_dr():
+    dir = "test_dir"
+    files = ["test1.txt","test2.txt","test3.txt"]
+    
+    os.mkdir(dir)
+    for file in files:
+        with open(f"{dir}/{file}", 'w') as _:
+            pass
         
-    def test_convert_to_datetime_raise_error_with_wrong_date_format(self):
-        with self.assertRaises(StringDateFormat):
-            convert_to_datetime("01.05.2022")
+    assert(set(get_files_from_dir(dir)) == set(files))
 
-class FileHandlerTests(unittest.TestCase): 
-
-    @classmethod
-    def setUpClass(cls):
-        cls.dir = "test_dir"
-        cls.files = ["test1.txt","test2.txt","test3.txt"]
-
-    def setUp(self):
-        os.mkdir(self.dir)
-        for file in self.files:
-            with open(f"{self.dir}/{file}", 'w') as _:
-                pass
-
-    def test_get_files_from_dir(self):
-        self.assertEqual(set(get_files_from_dir(self.dir)), set(self.files))
-        
-    def tearDown(self):
-        shutil.rmtree(self.dir)
-
-if __name__ == '__main__':  
-    unittest.main()
+    shutil.rmtree(dir)
